1d8808c8b6ce57e86e84136c0c8df66b1f6a8e20
root <root@localhost.localdomain>
2020-09-30 16:49:55 -0400

    updat

--- index.py
+++ index.py
@@ -1,15 +1,43 @@
 #!/usr/bin/env python
 
 import subprocess
-interface = input('interface > ')
-new_macAddress = input('new MAC > ')
-
-print('[+] Chinging MAC address for '+ interface + ' to '+ new_macAddress)
-
-subprocess.call(['ifconfig', interface, 'down'])
-subprocess.call(['ifconfig', interface, 'hw', 'ether', new_macAddress])
-subprocess.call(['ifconfig', interface, 'up'])
+import optparse
 
 
 
-#############-----
+def argument():
+    parsr = optparse.OptionParser()
+    parsr.add_option("-i", "--interface", dest="interface", help="add interface")
+    parsr.add_option("-m","--mac", dest="new_mac", help="add new mac")
+    (option, argument) = parsr.parse_args()
+    if not option.interface:
+        parsr.error("[-] plese spicfic interface")
+    elif not option.new_mac:
+        parsr.error("[-] please spisfic new mac")
+    return option
+
+
+
+
+def change_mac(interface, new_mac):
+    print("[+] change interface of "+interface+" and new mac is "+new_mac)
+    subprocess.call(["ifconfig",interface, "down"])
+    subprocess.call(["ifconfig", interface,"hw","ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+
+
+
+option = argument()
+change_mac(option.interface,option.new_mac)
+
+
+
+
+
+
+
+
+
+
